5b1099ebcc8962bda10838e3f49427133f439706
Jubril <18-30gd050@students.unilorin.edu.ng>
2022-08-05 01:53:29 +0100

    new

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,4 +1,12 @@
-const Button = ({onClick, color, text}) => {
-    return <button onClick={onClick} style={{backgroundColor: color}} className='btn'>{text}</button>
-}
-export default Button
+const Button = ({ onClick, color, text }) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className="btn 1"
+    > 
+      {text}
+    </button>
+  );
+};
+export default Button;
